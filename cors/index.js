9cf38e25e13a549f529a4056c440942db4882e8e
Kimbangul <highcolor_12@naver.com>
2023-05-08 08:08:19 +0900

    style: whitelist add

--- cors/index.js
+++ cors/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // PARAM 허용 url
-const whitelist = ["http://localhost:3000", "http://localhost:8000",];
+const whitelist = ["http://localhost:3000", "http://localhost:8000", "http://127.0.0.1:3000", "http://127.0.0.1:8000"];
 // PARAM 옵션
 const corsOptions = {
     origin: whitelist,
--- cors/index.ts
+++ cors/index.ts
@@ -2,7 +2,7 @@
 import cors from 'cors';
 
 // PARAM 허용 url
-const whitelist = ["http://localhost:3000", "http://localhost:8000",];
+const whitelist = ["http://localhost:3000", "http://localhost:8000","http://127.0.0.1:3000", "http://127.0.0.1:8000"];
 
 // PARAM 옵션
 const corsOptions : cors.CorsOptions = {
